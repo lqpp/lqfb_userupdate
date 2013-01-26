dd2d0aa874f42dd4b2e488aec2e5548a4824f829
Christian Fischer <sw-dev@computerlyrik.de>
2013-01-26 15:43:41 +0100

    adde logger facility

--- userupdate.py
+++ userupdate.py
@@ -4,7 +4,28 @@ import psycopg2
 import sys
 import csv
 import datetime
+import logging
 
+# create logger
+logger = logging.getLogger('userupdate')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+logger.info("Logger initialised")
+
+# Old school logfile (deprecated)
 logfile = "userupdate_log_" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.log'
 
 # parses csv file and returns array with data
