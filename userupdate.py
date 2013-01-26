d1be3e4e303023bfeaf01809dbdf3ca90e3c8c35
Reimerei <reimerei@lqpp.de>
2013-01-26 14:48:00 +0100

    some progress

--- userupdate.py
+++ userupdate.py
@@ -3,6 +3,9 @@
 import psycopg2
 import sys
 import csv
+import datetime
+
+logfile = "userupdate_log_" + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.log'
 
 # parses csv file and returns array with data
 def parse_file(filename):
@@ -18,6 +21,9 @@ def parse_file(filename):
 		for row in reader:
 			users.append(row)
 		
+	except:
+		sys.exit(1)
+
 	finally:
 		file.close()
 
@@ -35,7 +41,7 @@ def read_db(dbname):
 		con = psycopg2.connect(database = dbname, user = 'www-data') 
 		cur = con.cursor()
 
-		cur.execute("SELECT invite_code,id,identification FROM member ORDER BY invite_code")
+		cur.execute("SELECT invite_code,id,identification,locked FROM member ORDER BY invite_code")
 
 		users=cur.fetchall()	    
 
@@ -49,24 +55,71 @@ def read_db(dbname):
 
 	return users
 
+def logger(text) :
+	
+	log=open(logfile, 'a')
+
+	time=datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+	log.write(time + ": " + text)
+
+	print text
+
+	log.close()
+	
+def lock_user (user) :
+
+	logger("Locked user: " + user[0])
+
+
+def create_user (user) :
+
+	logger("Created user: " + user[0])
+
+def update_user (db_user, csv_user) :
+
+	logger("Updated user: " + db_user[0])
+
 
 
 # main program
 csv_users = parse_file("test.csv")
 db_users = read_db("lqfb_ap")
 
-# db_users: [invite,user_id,identification]
+# db_users: [invite,user_id,identification,locked]
 # csv_users = [invite, identification, Land, Kreis, Regional, darf abstimmen]
 
 
 # loop through the users in the DB and check if they are in the csv
-for user in db_users :
-	
-	
+csv_index = 0
+
+for db_index in len(db_users) :
+
+	user = db_users[db_index]
+
+	csv_start = csv_index
+
+	# find user in csv
+	while user[0] != csv_users[csv_index][0] :
+
+		csv_index += 1 
+
+
+	if user[0] == csv_users[csv_index][0] :
+		# we found the user
+		update_user(user, csv_users[csv_index])
+
+	else :
+		# user is not in the csv so lock him
+		csv_index = csv_start
+		lock_user(user)
 
 
 
 
 
-print csv_users
-print db_users
+
+
+
+#print csv_users
+#print db_users
